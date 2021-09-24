1de853e70d6a69ded8065a6d3d750292f1fe2605
Daniel Zoba <daniel.sobe@sorben.com>
2021-09-24 15:15:59 +0200

    add another overlay for the updater URL

--- product.mk
+++ product.mk
@@ -1,3 +1,6 @@
 
+# Overlays
+PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
+
 # supply a useful URL to the running OTA container
 PRODUCT_PROPERTY_OVERRIDES += lineage.updater.uri=https://eosupdate.serbski-inkubator.de/api/v1/{device}/{type}/{incr}
